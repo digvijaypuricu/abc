a8d7139f90f482f769d9fa2d9d8955be0c8d381c
digvijaypuricu <digvijay.e10051@cumail.in>
2024-03-31 14:50:38 +0530

    Welcome

---
+++ area.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+// Function to calculate the area of a rectangle
+double calculateArea(double length, double width) {
+    return length * width;
+}
+
+int main() {
+    double length, width;
+
+    // Input length and width of the rectangle
+    cout << "Enter the length of the rectangle: ";
+    cin >> length;
+    cout << "Enter the width of the rectangle: ";
+    cin >> width;
+
+    // Calculate the area
+    double area = calculateArea(length, width);
+
+    // Print the result
+    cout << "Area of the rectangle: " << area << endl;
+
+    return 0;
+}
